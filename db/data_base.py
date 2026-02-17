e58afb5ca7162e5238a875c27fec849540baad06
Shivangkukreti <shivangkukretiwork@gmail.com>
2026-02-17 22:09:51 +0530

    ok

--- db/data_base.py
+++ db/data_base.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 MONGO_URL = os.getenv("MONGO_URL")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")  
-
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[MONGO_DB_NAME]
 fruits_collection = db.get_collection("fruits")
